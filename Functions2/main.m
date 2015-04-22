97ef8eae8d903bc632cf3b35e5f41ad489ba406b
Jared <jaredtaylor@cbrc.net>
2015-04-22 10:12:44 -0400

    functions for exponents and areas

--- Functions2/main.m
+++ Functions2/main.m
@@ -8,10 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+int calcExp(int val, int exp){
+    int result = 1;
+    int i = 0;
+    for(i = 0; i < exp; i++){
+        result = result * val;
+    }
+    return result;
+}
+float calcArea(int radius){
+    int radiusSquared = calcExp(radius, 2);
+    return 3.14 * (float) radiusSquared;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        float i = calcArea(7);
+        NSLog(@"%f", i);
     }
     return 0;
 }
